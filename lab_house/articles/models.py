f489d3932153444286620676d72c74305386e830
justddreamm <lagovsp@gmail.com>
2021-11-01 17:12:43 +0300

    object added to db

--- lab_house/articles/models.py
+++ lab_house/articles/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 
 # describing the subject here
-class Article(models.Model):
+class Articles(models.Model):
 	title = models.CharField(max_length = 255)
 	content = models.TextField(blank = True)  # blank = True means can be empty
 	photo = models.ImageField(upload_to = 'photos/%y/%m/%d')
 	time_created = models.TimeField(auto_now_add = True)  # accepts current time when creating and will noe change
 	time_updated = models.TimeField(auto_now = True)  # refreshes every time we update
 	is_published = models.BooleanField(default = True)  # default value is True
+
+	def __str__(self):
+		return self.title
