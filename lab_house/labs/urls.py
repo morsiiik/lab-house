40a5d7b0b3fa643224df5f8c9dde9459fad6bad0
morsiiik <ognezvezd02@mail.ru>
2021-11-02 22:23:34 +0300

    added html samples and css

--- lab_house/labs/urls.py
+++ lab_house/labs/urls.py
@@ -4,5 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('labs/<int:lab_number>/', lab_num),  # http://127.0.0.1:8000/labs/
-    path('hello/', hello)
+    path('hello/', hello),
+    path('', index, name='home'),
+    path('about/', about, name='about'),
+    path('redirect/', redir)
 ]
--- lab_house/labs/views.py
+++ lab_house/labs/views.py
@@ -1,10 +1,22 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .models import *
+
+menu = ["About site", "Add comment", "Sign in"]
 
 def hello(request):
     return HttpResponse("Hello")
 
+def redir(request):
+    return redirect('home', permanent=True)
+
+def index(request):
+    posts = Lab.objects.all()
+    return render(request, 'labs/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+
+def about(request):
+    return render(request, 'labs/about.html', {'menu': menu, 'title': 'О сайте'})
 
 def lab_num(request, lab_number):
     return HttpResponse(f"<h1>Лаба</h1><p>{lab_number}</p>")
